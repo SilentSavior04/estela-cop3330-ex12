e58976c52f5b34a63d625354e11d5fdb0afb1216
Christian Estela <cestela3@gmail.com>
2021-09-10 14:15:11 -0400

    Initial commit

---
+++ src/com/company/ComputeSimpleInterest.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.Scanner;
+
+public class ComputeSimpleInterest
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Principle Amount");
+        int p = sc.nextInt();
+        System.out.println("Compound Rate");
+        float r = sc.nextFloat();
+        System.out.println("Number of Years");
+        int t = sc.nextInt();
+        System.out.println("Number of times the interest is compounded per year");
+        int n = sc.nextInt();
+        //int t1= n*t;
+        double a = (double) p * (Math.pow((1 + r/n), n*t));
+        System.out.println("$" + p + " invested at " +r+"% for " +t+ " years compounded " +n+ " times per year is" + "$" + a);
+    }
+}
